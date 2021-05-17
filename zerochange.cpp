10d6a892d61c813c002b3f206fc233a87a857037
Latha <84034765+latha558@users.noreply.github.com>
2021-05-17 21:24:03 +0530

    Create zerochange.cpp

---
+++ zerochange.cpp
@@ -0,0 +1,54 @@
+// User function Template for C++
+class
+ Solution
+ {
+ 
+public:
+    long long int convertEvenBitToZero(long long int n)
+ {
+    int to_subtract = 0;
+  
+  int m = 0;
+  
+  for (int x = n; x; x >>= 2)
+ {
+      
+  if (x & 1)
+         
+   to_subtract += (1 << m);
+     
+   m += 2;
+  
+  } 
+    
+return n - to_subtract;
+
+}
+    
+};
+// { Driver Code Starts.
+
+int main()
+ {
+   
+ int t;
+    
+cin >> t;
+   
+ while (t--)
+{
+        
+long long int n;
+    
+    cin >> n;
+   
+     Solution ob;
+       
+ cout << ob.convertEvenBitToZero(n) << endl;
+  
+  }
+  
+  return 0;
+}
+ 
+ // } Driver Code Ends
